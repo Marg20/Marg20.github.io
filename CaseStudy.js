b4209c62c0b344959aa8aca1beb58b9254cd0036
Marg20 <89118525+Marg20@users.noreply.github.com>
2022-03-29 23:22:58 -0600

    initial commit

---
+++ CaseStudy.js
@@ -0,0 +1,26 @@
+function viewResumeFunction () {
+	window.location.replace("MargPapakostaResume.pdf");
+}
+const WhatsCookinclick = document.getElementById("WhatsCookinApp");
+
+/*WhatsCookinclick.onclick = function() {
+	window.location.replace("whatscookin.html");
+}*/
+
+const HSUclick = document.getElementById("HSU");
+
+HSUclick.onclick = function() {
+	window.location.replace("hsu.html");
+}
+
+/*const HappyDogclick = document.getElementById("HappyDogApp");
+
+HappyDogclick.onclick = function() {
+	window.location.replace("happydog.html");
+}
+
+const FTCclick = document.getElementById("FTCSmart");
+
+FTCclick.onclick = function() {
+	window.location.replace("ftc.html");
+}*/
---
+++ Scripts.js
@@ -0,0 +1,36 @@
+window.onload = function() {
+	document.getElementById("footer").innerHTML = `<div class="page-footer"><span><a href="https://www.instagram.com/margaritaines/" target="_blank"><img src="images/InstagramIcon.svg" alt="Instagram Icon" /></a></span>
+	<span><a href="https://www.linkedin.com/in/mpapakosta/" target="_blank"><img src="images/LinkedInIcon.svg" alt="LinkedIn Icon" /></a></span>
+	<span><a href="mailto:margaritainespap@gmail.com"><img src="images/EmailIcon.svg" alt="Email Icon" /></a></span>
+	<span><a href="https://www.facebook.com/margarita.ines.5" target="_blank"><img src="images/FacebookIcon.svg" alt="Facebook Icon" /></a></span></div>`
+}
+
+function moreAboutMeFunction () {
+	window.location.replace("about.html");
+}
+
+const MyLogo = document.getElementById("MyLogo");
+
+MyLogo.onclick = function() {
+	window.location.replace("index.html");
+}
+
+
+
+
+
+
+ const next = document.querySelector(".carousel__btn--next"),
+ back = document.querySelector(".carousel__btn--back"),
+ carousel = document.querySelector(".carousel__cards");
+let angle = 0;
+
+next.addEventListener("click", () => {
+  angle -= 45;
+  carousel.style.transform = `translateZ(-25rem) rotateY(${angle}deg)`;
+});
+
+back.addEventListener("click", () => {
+  angle += 45;
+  carousel.style.transform = `translateZ(-25rem) rotateY(${angle}deg)`;
+});
