b643f972379c5a457ad8ffaf5bd666b6c45566fb
Marg20 <89118525+Marg20@users.noreply.github.com>
2022-05-23 00:44:05 -0600

    adding art html, css, and js

---
+++ ArtScripts.js
@@ -0,0 +1,23 @@
+const Digitalclick = document.getElementById("DigitalArt");
+
+Digitalclick.onclick = function() {
+	window.location.replace("digital.html");
+}
+
+const Paintingclick = document.getElementById("Painting");
+
+Paintingclick.onclick = function() {
+	window.location.replace("painting.html");
+}
+
+const Photographyclick = document.getElementById("Photography");
+
+Photographyclick.onclick = function() {
+	window.location.replace("photography.html");
+}
+
+const Recipesclick = document.getElementById("Recipes");
+
+Recipesclick.onclick = function() {
+	window.open('https://express.adobe.com/page/la2DVCEAcBqSP/', '_blank');
+}
